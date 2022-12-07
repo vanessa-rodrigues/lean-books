dcadc8b3f665eda5c6d7153384bd152a9c1ca483
Vanessa Rodrigues <vanessa.rodrigues@microsoft.com>
2022-12-07 10:00:34 -0300

    Updates until 07/12/2022

--- functional_programming/functional_programming_5.lean
+++ functional_programming/functional_programming_5.lean
@@ -118,8 +118,8 @@ def ReaderOption (ρ : Type) (α : Type) : Type := ρ → Option α
 
 def ReaderOption.pure (x : α) : ReaderOption ρ (Option α) := fun _ => some x
 
--- def ReaderOption.bind (result : ReaderOption ρ (Option α)) (next : α → ReaderOption ρ β) : ReaderOption ρ β :=
---   fun env => next (result env) env
+def ReaderOption.bind (result : ReaderOption ρ (Option α)) (next : α → ReaderOption ρ (Option β)) : ReaderOption ρ (Option β) :=
+  fun env => sorry
 
 def ReaderExcept (ε : Type) (ρ : Type) (α : Type) : Type := ρ → Except ε α
 
@@ -128,3 +128,107 @@ abbrev Env : Type := List (String × (Int → Int → Int))
 def exampleEnv : Env := [("max", max), ("mod", (· % ·))]
 
 #check ReaderOption.pure 6
+
+-- 5.3
+inductive Expr (op : Type) where
+  | const : Int → Expr op
+  | prim : op → Expr op → Expr op → Expr op
+
+
+inductive Arith where
+  | plus
+  | minus
+  | times
+  | div
+
+inductive Prim (special : Type) where
+  | plus
+  | minus
+  | times
+  | other : special → Prim special
+
+inductive CanFail where
+  | div
+
+def divOption : CanFail → Int → Int → Option Int
+  | CanFail.div, x, y => if y == 0 then none else pure (x / y)
+
+def divExcept : CanFail → Int → Int → Except String Int
+  | CanFail.div, x, y =>
+    if y == 0 then
+      Except.error s!"Tried to divide {x} by zero"
+    else pure (x / y)
+
+def applyPrim [Monad m] (applySpecial : special → Int → Int → m Int) : Prim special → Int → Int → m Int
+  | Prim.plus, x, y => pure (x + y)
+  | Prim.minus, x, y => pure (x - y)
+  | Prim.times, x, y => pure (x * y)
+  | Prim.other op, x, y => applySpecial op x y
+
+-- def evaluateM [Monad m] (applySpecial : special → Int → Int → m Int): Expr (Prim special) → m Int
+--   | Expr.const i => pure i
+--   | Expr.prim p e1 e2 =>
+--     evaluateM applySpecial e1 >>= fun v1 =>
+--     evaluateM applySpecial e2 >>= fun v2 =>
+--     applyPrim applySpecial p v1 v2
+
+def evaluateM [Monad m] (applySpecial : special → Int → Int → m Int): Expr (Prim special) → m Int
+  | Expr.const i => pure i
+  | Expr.prim p e1 e2 => do 
+    let v1 <- evaluateM applySpecial e1
+    let v2 <- evaluateM applySpecial e2
+    (applyPrim applySpecial p v1 v2)
+
+def applyEmpty [Monad m] (op : Empty) (_ : Int) (_ : Int) : m Int :=
+  nomatch op
+
+open Expr Prim in
+#eval evaluateM (m := Id) applyEmpty (prim plus (const 5) (const (-14)))
+
+----------------------------
+instance : Monad Option where
+  pure x := some x
+  bind opt next :=
+    match opt with
+    | none => none
+    | some x => next x
+
+instance : Monad (Except ε) where
+  pure x := Except.ok x
+  bind attempt next :=
+    match attempt with
+    | Except.error e => Except.error e
+    | Except.ok x => next x
+
+
+-- def firstThirdFifthSeventh [Monad m] (lookup : List α → Nat → m α) (xs : List α) : m (α × α × α × α) :=
+--   lookup xs 0 >>= fun first =>
+--   lookup xs 2 >>= fun third =>
+--   lookup xs 4 >>= fun fifth =>
+--   lookup xs 6 >>= fun seventh =>
+--   pure (first, third, fifth, seventh)
+
+def firstThirdFifthSeventh [Monad m] (lookup : List α → Nat → m α) (xs : List α) : m (α × α × α × α) := do
+  let first <- lookup xs 0 
+  let third <- lookup xs 2
+  let fifth <- lookup xs 4
+  let seventh <- lookup xs 6
+  pure (first, third, fifth, seventh)
+
+def slowMammals : List String :=
+  ["Three-toed sloth", "Slow loris"]
+
+def fastBirds : List String := [
+  "Peregrine falcon",
+  "Saker falcon",
+  "Golden eagle",
+  "Gray-headed albatross",
+  "Spur-winged goose",
+  "Swift",
+  "Anna's hummingbird"
+]
+
+#eval firstThirdFifthSeventh (fun xs i => xs[i]?) slowMammals
+
+#eval firstThirdFifthSeventh (fun xs i => xs[i]?) fastBirds
+
